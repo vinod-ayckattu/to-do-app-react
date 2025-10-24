5edd2697584dc2674dc1e79d3fa1dd1ed9a0f979
vinod-ayckattu <vinodharidas.dev24@gmail.com>
2025-10-24 18:35:33 +0530

    router-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
